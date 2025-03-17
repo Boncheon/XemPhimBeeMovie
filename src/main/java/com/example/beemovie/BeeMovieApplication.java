09c09c72185b775c525f662347a781144d5ccce5
dmdmdmmdmmdm <manh56823@gmail.com>
2025-03-18 00:45:26 +0700

    test

---
+++ src/main/java/com/example/beemovie/BeeMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.beemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beemovie/BeeMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
